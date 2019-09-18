d5a8b0d6636825cdb83d9409ce96d3b0394a26fa
unknown <shmtomer@gmail.com>
2019-09-18 10:31:40 -1000

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const bodyParser = require ('body-parser')
+const api = require ('./server/routes/api')
+const mongoose = require ('mongoose')
+const path = require ('path')
+
+const app = express()
+const port = 4000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+mongoose.connect('mongodb://localhost/CitiesDB',{useNewUrlParser:true})
+
+app.use(express.static(path.join(__dirname,'./dist')))
+app.use(express.static(path.join(__dirname,'./node_modules')))
+
+app.use('/',api)
+
+app.listen(port,()=>console.log(`running on ${port}`))
