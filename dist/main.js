fb0f3b6bca596f7cc9980406f477d425ea107285
JovanDyaz <jvaonam@me.com>
2019-09-19 00:47:20 -0500

    frontEndReborn

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,12 @@
-
-const routeManager = new RouteManager()
 const renderer = new Renderer()
 
-//javascript (intro)
-// const inputIntro = document.getElementById("city-input")
-// inputIntro.addEventListener("keyup", function (event) {
-    //     if (event.keyCode === 13) {
-//         event.preventDefault()
-//         document.getElementById("button").click()
-//     }
-// })
-// navigator.geolocation.getCurrentPosition(success,error)
+M.AutoInit();
+
+$(document).ready(function () {
+    $('.slider').slider();
+});
 
 const handleSearch = async function(){
     let input = $(".location").val()
     await routeManager.getLocation(input)
 }
-
