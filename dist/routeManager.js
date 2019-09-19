87c8aa55514fc9bd33e65a7e0f1a07d46fa6c0d6
JovanDyaz <jvaonam@me.com>
2019-09-19 14:42:55 -0500

    picMap

--- dist/routeManager.js
+++ dist/routeManager.js
@@ -3,14 +3,6 @@ class RouteManager {
         this.locations = []
     }
     
-    initMap(lat,lng) {
-        var location = {lat: lat, lng: lng};
-        
-        var map = new google.maps.Map(
-            document.getElementById('map'), {zoom: 12, center: location});
-        
-        var marker = new google.maps.Marker({position: location, map: map});
-    }
     async getLocation(location){
         let data = await $.get(`location/${location}`)
         let validator = this.locations.find(l =>l.name==location.name)
