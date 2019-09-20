6a3b0ecb252be727d0bfc22757b3ea5cdc98207e
JovanDyaz <jvaonam@me.com>
2019-09-20 04:00:30 -0500

    fixing master

--- dist/main.js
+++ dist/main.js
@@ -27,11 +27,11 @@ const handleSearch = async function () {
 }
 
 const logIn = function () {
-function initMap(lat, lng) {
-    let point = { lat, lng }
-    let map = new google.maps.Map(document.getElementById('map'), { center: point, zoom: 14 })
-    let marker = new google.maps.Marker({ position: point, map: map })
-
+    const username = $("#icon_prefixN").val()
+    const password = $("#icon_prefixP").val()
+    let logInFlag = true
+    renderer.renderLI(logInFlag)
+    routeManager.signUp({ username, password })
 }
 
 function initMap(lat, lng) {
@@ -42,10 +42,10 @@ function initMap(lat, lng) {
 }
 const findMe = function () {
     function success(position) {
-        routeManager.point.lat = position.coords.latitude
-        routeManager.point.lng = position.coords.longitude
+        const lat = position.coords.latitude
+        const lng = position.coords.longitude
 
-        initMap(routeManager.point.lat,routeManager.point.lng)
+        initMap(lat, lng)
     }
     function geo_error() {
         alert("Sorry, no position available.");
@@ -62,4 +62,3 @@ const findMe = function () {
 setTimeout(() => {
     findMe()
 }, timeout = 1000);
-
